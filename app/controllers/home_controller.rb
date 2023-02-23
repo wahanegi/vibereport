8b03f456da61409b340713e84c0e94a101e7ec0a
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-02-23 17:11:28 +0000

    CI-35: fix AA

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  include ApplicationHelper
   before_action :require_user!, only: :app
 
   def index
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  include Passwordless::ControllerHelpers
+
+  private
+
+  def current_user
+    @current_user ||= authenticate_by_session(User)
+  end
+
+  def require_user!
+    return if current_user
+
+    redirect_to root_path, flash: { error: 'You are not worthy!' }
+  end
 end
