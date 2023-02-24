81697115a2ab3a35cb8e5176a1c2b0a69d945743
VitaliiShevchenko <iksel@ukr.net>
2023-02-24 21:03:53 +0200

    CI-36: edit configuration of passwordless

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -1,11 +1,8 @@
 Passwordless.default_from_address = "do_not_reply@#{ENV.fetch('EMAIL_DOMAIN')}"
-if Rails.env.production?
-  # entry point of React
-  Passwordless.success_redirect_path = "https://#{ENV.fetch('EMAIL_DOMAIN')}/app"
-  Passwordless.failure_redirect_path = "https://#{ENV.fetch('EMAIL_DOMAIN')}/home"
-else
-  url = Rails.application.config.action_mailer.default_url_options
-  url_port = "#{url[:host]}:#{url[:port]}"
-  Passwordless.success_redirect_path = "http://#{url_port}/temporary_blank_page"
-  Passwordless.failure_redirect_path = "http://#{url_port}/home"
-end
+Passwordless.failure_redirect_path = "/home"
+
+  # entry point of React. When React will be ready please uncomment
+  # Passwordless.success_redirect_path = "/app"
+
+  # When will be work next step(React page) please comment it
+  Passwordless.success_redirect_path = "/temporary_blank_page"
