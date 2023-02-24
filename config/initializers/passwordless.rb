f2131e871ec510be82c69c7da6820a720cbf0a73
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-02-24 19:33:26 +0000

    CI-38: change routes and add expire

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -1,8 +1,4 @@
 Passwordless.default_from_address = "do_not_reply@#{ENV.fetch('EMAIL_DOMAIN')}"
-Passwordless.failure_redirect_path = "/home"
-
-  # entry point of React. When React will be ready please uncomment
-  # Passwordless.success_redirect_path = "/app"
-
-  # When will be work next step(React page) please comment it
-  Passwordless.success_redirect_path = "/temporary_blank_page"
+Passwordless.failure_redirect_path = "/"
+Passwordless.success_redirect_path = "/app"
+Passwordless.expires_at = lambda { 1.week.from_now }
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   passwordless_for :users, at: '/', as: :auth
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-
-  # Temporary routes
-  get '/temporary_blank_page', to: 'home#temporary_sign_in_blank_page_for_test', as: :temporary_blank_page
-
   get '/app', to: 'home#app', as: :app
+
   root to:"home#index"
 end
