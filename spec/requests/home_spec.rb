4425a6565e80adc3571001ba0d16fa394cf18a7a
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-02-26 10:29:27 +0000

    CI-38: fix tests

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
+require "passwordless/test_helpers"
 
 RSpec.describe "Homes", type: :request do
+  let!(:user) { create :user }
+  before(:each) do
+    passwordless_sign_in(user)
+  end
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+    it "returns http found" do
+      get '/'
+      expect(response).to have_http_status(:found)
     end
   end
 
   describe "GET /app" do
     it "returns http success" do
-      get "/home/app"
+      get "/app"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
