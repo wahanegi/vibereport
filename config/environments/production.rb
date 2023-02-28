352fd7f5f32140062892a1eb3b8099b5a4622184
Roger Graves <rogergraves@gmail.com>
2023-02-28 19:56:22 +0000

    CI-14: Configure production environment for mail service setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Settings for external email services such as Mailtrap or SendGrid
+  config.action_mailer.asset_host = "http://#{ENV['DOMAIN_URL']}"
+  config.action_mailer.default_url_options = { host: ENV['DOMAIN_URL'] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    address: ENV['SMTP_ADDRESS'],
+    domain: ENV['SMTP_DOMAIN'],
+    port: ENV['SMTP_PORT'],
+    authentication: (ENV['SMTP_AUTHENTICATION'].present? ? ENV['SMTP_AUTHENTICATION'].to_sym : 'plain')
+  }
 end
