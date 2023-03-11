0dfce7fa1c55d567b2fbd453eff7d23cf7f0f5a9
VitaliiShevchenko <iksel@ukr.net>
2023-03-11 19:57:39 +0200

    improve MemeSelection

--- app/javascript/components/Pages/Hierarchy.js
+++ app/javascript/components/Pages/Hierarchy.js
@@ -6,23 +6,43 @@ import EmotionEntry from "./EmotionEntry";
 import SelectedGIPHYFollow from "./SelectedGIPHYFollow";
 import OwnMemeUploadFollow from "./OwnMemeUploadFollow";
 import FollowUpPosWordOnly from "./FollowUpPosWordOnly";
+import {apiRequest} from "../requests/axios_requests";
+import {mergeData} from "../helper_functions/library";
+import {useNavigate} from "react-router-dom";
 
 const Hierarchy = ({step, data, setData}) => {
+  const navigate = useNavigate()
+
+  //*** **setError** - Hook for handling error messages
+  //*** **steps** - array with steps of user for update in DB
+  //*** **addedData** - necessary data (and future data) for update in DB by using Response controller
+  //*** Format addedData = **{key: value, ...., key(n): value(n)}**
+  const saveDataToDb = (setError, steps, addedData = {}) =>{
+    const dataRequest = {response:{step: JSON.stringify(steps),...addedData}}
+    apiRequest("PATCH", dataRequest, saveDataToAttributes).catch(e=>setError(e))
+  }
+
+  //***  include received data from the apiRequest to the variable **:data** (**:emotionDataRespUserIdTimePeriod** in App)
+  const saveDataToAttributes =( receivedData ) =>{
+    mergeData( receivedData, data, setData )
+    console.log("navigate", receivedData.attributes.step)
+    navigate(`/${JSON.parse(receivedData.attributes.step).pop()}`)
+  }
 
   console.log("<Hierarchy >", data, "</Hierarchy>")
   switch (step) {
     case  "ListEmotions" :
-      return <ListEmotions data={data}  setData={setData}/>
+      return <ListEmotions data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     case  "MemeSelection" :
-      return <MemeSelection data={data}  setData={setData}/>
+      return <MemeSelection data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     case  "EmotionEntry" :
-      return <EmotionEntry data={data}  setData={setData}/>
+      return <EmotionEntry data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     case  "SelectedGIPHYFollow" :
-      return <SelectedGIPHYFollow data={data}  setData={setData}/>
+      return <SelectedGIPHYFollow data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     case  "OwnMemeUploadFollow" :
-      return <OwnMemeUploadFollow data={data}  setData={setData}/>
+      return <OwnMemeUploadFollow data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     case  "FollowUpPosWordOnly" :
-      return <FollowUpPosWordOnly data={data}  setData={setData}/>
+      return <FollowUpPosWordOnly data={data}  setData={setData} saveDataToDb={saveDataToDb}/>
     // case  "FollowUpPosMeme" :
     //   return <FollowUpPosMeme data={data} setData={setData} />
     // case  "ProductivityCheckLow" :
--- app/javascript/components/Pages/MemeSelection.js
+++ app/javascript/components/Pages/MemeSelection.js
@@ -1,11 +1,6 @@
 import React, {Fragment, useEffect, useState} from "react"
-import * as ReactRouterDOM from "react-router-dom"
 import {Button} from "react-bootstrap";
-import {redirect, useNavigate, useParams} from "react-router-dom";
-import {apiRequest, updateResponse} from "../requests/axios_requests";
-import {isEmpty} from "../helpers/helper";
 import BackButton from "../UI/BackButton";
-import {mergeData} from "../helper_functions/library";
 
 //*** Below what we have in the data after ListEmotion(example). See variable **emotionDataRespUserIdTimePeriod** in the App.js
 //***               data: {Emotions:{id:..., type:..., attributes:{ word:..., category:... }},
@@ -17,51 +12,32 @@ import {mergeData} from "../helper_functions/library";
 //***                                      id: 1},
 //***               current_user_id: 1,
 //***               time_period:{...}
-const MemeSelection = ({data, setData}) => {
-  const [response, setResponse] = useState({})
+const MemeSelection = ({data, setData, saveDataToDb}) => {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(false)
 
-  const navigate = useNavigate();
   const steps = JSON.parse(data.response.attributes.step)
   const emotionId = data.response.attributes.emotion_id
   const emotionWord = data.response.attributes.word
 
   const handlingOnClickSkip = () =>{
+    setIsLoading(true)
     steps.push('FollowUpPosWordOnly')
-    saveDataToDb( steps ,{})
-    }
-    const saveDataToDb = (steps, addedData = {}) =>{
-      const dataRequest = {response:{step: JSON.stringify(steps),...addedData}}
-      setIsLoading(true)
-      //request to the Response controller
-      //update record in the Response table
-      apiRequest("PATCH", dataRequest, saveDataToAttributes).catch(e=>setError(e))
+    saveDataToDb( setError, steps ,{})
     }
 
-  //***  include received data from the apiRequest to the variable **:data** (**:emotionDataRespUserIdTimePeriod** in App)
-  const saveDataToAttributes =( receivedData ) =>{
-    mergeData( receivedData, data, setData )
-    navigate(`/${JSON.parse(data.response.attributes.step).pop()}`)
-    setIsLoading(false)
-  }
-
-  // useEffect(()=>{
-  //   if(!isEmpty(response)){
-  //     console.log("skipHandling useEffect steps=", response.attributes)
-  //     navigate(`/${JSON.parse(data.response.attributes.step).pop()}`);
-  //     // window.location.replace(`/${JSON.parse(response.attributes.step).pop()}`);
-  //   }
-  // },[response])
   const chooseGIPHYHandling=()=>{
     steps.push('SelectedGIPHYFollow')
-    saveDataToDb( steps ,{})
+    saveDataToDb( setError, steps ,{})
   }
   const uploadGIPHYHandling=()=>{
     steps.push('OwnMemeUploadFollow')
-    saveDataToDb( steps ,{})
+    saveDataToDb( setError, steps ,{})
   }
 
+  useEffect(()=>{
+    setIsLoading(false)
+  },[data])
 
   return(
     <Fragment>
