68f45ae2cc9b3c27ba490837f3e601e77915aeda
Roger Graves <rogergraves@gmail.com>
2023-03-14 17:37:10 +0000

    CI-14: Add setting for production mail to use Postmark.app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,11 +96,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['DOMAIN_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
-    address: ENV['SMTP_ADDRESS'],
-    domain: ENV['SMTP_DOMAIN'],
-    port: ENV['SMTP_PORT'],
-    authentication: (ENV['SMTP_AUTHENTICATION'].present? ? ENV['SMTP_AUTHENTICATION'].to_sym : 'plain')
+    authentication: (ENV['SMTP_AUTHENTICATION'].present? ? ENV['SMTP_AUTHENTICATION'].to_sym : 'plain'),
+    enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] == 'true'
   }
 end
