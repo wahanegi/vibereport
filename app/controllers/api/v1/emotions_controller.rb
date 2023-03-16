50cea922f32ae976f01bcee369a319ec97088d8c
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-03-16 19:39:33 +0000

    CI-50: small fix

--- app/controllers/api/v1/emotions_controller.rb
+++ app/controllers/api/v1/emotions_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::EmotionsController < ApplicationController
         start_date: TimePeriod.current.start_date,
         end_date: TimePeriod.current.end_date
       },
-      response: @current_response ? response_hash : { attributes: { steps: %w[emotion-selection-web] } },
+      response: @current_response ? response_hash : { attributes: { steps: %w[emotion-selection-web].to_s } },
       emotion: @current_response ? Emotion.find_by(id: @current_response.emotion_id) : '{}'
     }
   end
