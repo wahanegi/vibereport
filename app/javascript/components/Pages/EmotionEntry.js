e0ed2dfac257d2cb96b540b2d9a38382316246af
Maryna-Harasko <mngarasko@gmail.com>
2023-03-16 11:22:33 +0200

    CI-44 feature Emotion Entry

--- app/javascript/components/Pages/EmotionEntry.js
+++ app/javascript/components/Pages/EmotionEntry.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import { Button } from "react-bootstrap";
+import { createEmotion } from "../requests/axios_requests";
+import Menu from "../UI/Menu";
+import iconNegative from "../../../assets/images/icon_negative.svg";
+import iconNeutral from "../../../assets/images/icon_neutral.svg";
+import iconPositive from "../../../assets/images/icon_positive.svg";
+import QuestionButton from "../UI/QuestionButton";
+import ShoutoutButton from "../UI/ShoutoutButton";
 
 const EmotionEntry = () => {
-  return <div>
-    <h2>Emotion Entry Page</h2>
-    <p>No emotion was clicked</p>
-  </div>
+  const [emotion, setEmotion] = useState({ word: '', category: '' });
+  const onChangeEmotion = (e) => {
+    setEmotion(Object.assign({}, emotion, {[e.target.name]: e.target.value}))
+  }
+  const emojis = [
+    { name: 'negative', icon: <img className="icon-negative" src={iconNegative} alt={"negative"}/> },
+    { name: 'neutral', icon: <img className="icon-neutral" src={iconNeutral} alt={"neutral"}/> },
+    { name: 'positive', icon: <img className="icon-positive" src={iconPositive} alt={"positive"}/> },
+  ];
+  const [hoveredEmoji, setHoveredEmoji] = useState("");
+  const handleEmojiClick = (name) => {
+    setSelectedEmoji(name);
+    setEmotion(prevState => ({...prevState, category: name}));
+    setHoveredEmoji("");
+  };
+  const saveEmotion = () => {
+    createEmotion(emotion, setEmotion)
+  };
+  const [selectedEmoji, setSelectedEmoji] = useState("");
+  const getEmojiClass = (category, name) => {
+    if (category === "positive") {
+      return "input-positive";
+    } else if (category === "negative") {
+      return "input-negative";
+    } else if (category === "neutral") {
+      return "input-neutral";
+    } else {
+      return "";
+    }
+  };
+
+  return <Fragment>
+    <div className="convert increased-convert in_left">
+      <p>Logo/Brand</p>
+      <div className="line1 offset-line1"></div>
+      <div className="line2 offset-line2"></div>
+    </div>
+    <div className="question q-emotion-entry">A new one! What’s up?</div>
+    <h3 className="over-input-entry">What word best describes your week?</h3>
+    <Form.Control className ={`${getEmojiClass(selectedEmoji)} email_field input_new-word`} size="lg" type="text" placeholder="Add a new word" name="word" value={emotion.word || ''} onChange={onChangeEmotion} />
+    <h3 className="under-input-entry">How do you feel about this word?</h3>
+    <div className="wrap-emoji">
+      {emojis.map((emoji) => (
+          <div
+              className={`wrap-icon ${emoji.name}-icon`}
+              key={emoji.name}
+              onClick={() => handleEmojiClick(emoji.name)}
+          >
+            {emoji.icon} <span>{emoji.name}</span>
+          </div>
+      ))}
+    </div>
+    <Button>Go back</Button>
+    <Button onClick={saveEmotion}>Next</Button>
+    <QuestionButton style={{position: 'absolute', right: 47}}/>
+    <ShoutoutButton style={{position: 'absolute', left: 45}}/>
+    <Menu style={{position: 'absolute', right: 47, top: 62}}>X% complete</Menu>
+  </Fragment>
 }
 export default EmotionEntry;
