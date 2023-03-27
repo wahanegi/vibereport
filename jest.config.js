5657acea90a12eb0f1ddb116160623daefbf845d
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-03-27 10:30:56 +0100

    CI-83: add jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx', 'svg'],
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.svg$": "<rootDir>/svgTransform.js"
   },
   testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   moduleNameMapper: {
