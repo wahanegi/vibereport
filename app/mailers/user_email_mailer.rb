b99880ca797d5bc453cccd66dfa3f05f86903fcb
VitaliiShevchenko <iksel@ukr.net>
2023-04-07 20:31:01 +0300

    CI-70: Changed with tooltips of the codeclimate

--- app/mailers/user_email_mailer.rb
+++ app/mailers/user_email_mailer.rb
@@ -6,9 +6,9 @@ class UserEmailMailer < ApplicationMailer
   NUMBER_OF_ELEMENTS = Emotion::SHOW_NUMBER_PER_CATEGORY
 
   def response_invite(user, time_period)
-    @user = user
+
     general_link = { controller: 'api/v1/responses', action: 'response_flow_from_email',
-                      time_period_id: TimePeriod.current, not_working: false, user_id: user.id }
+                     time_period_id: TimePeriod.current, not_working: false, user_id: user.id }
     @link_for_own_word = [general_link, { last_step: 'emotion-entry' }].reduce(:merge)
     @link_for_was_not  = [general_link, { last_step: 'results' }].reduce(:merge)
     @link_for_emotion  = [general_link, { emotion_id: nil, last_step: 'meme-selection' }].reduce(:merge)
