7ca5f2665fde086c86d09274d84551a4bc23b4d3
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-04-10 10:52:46 +0200

    CI-154: fix styles for MemeSelection page

--- app/javascript/components/Pages/MemeSelection.js
+++ app/javascript/components/Pages/MemeSelection.js
@@ -44,7 +44,7 @@ const MemeSelection = ({data, setData, saveDataToDb, steps, service, isCustomGif
   const Navigation = () =>
     <div className='d-flex justify-content-between gap-3'>
       <div>
-        <h5 className='mt-2 mb-0'>Share it in your way!</h5>
+        <h5 className='mt-2 mb-0 text-muted'>Share it in your way!</h5>
         <BtnOutline text='Upload your own meme!' onClick={uploadGIPHYHandling} />
       </div>
       <div style={{marginTop: 52}}>
@@ -56,7 +56,7 @@ const MemeSelection = ({data, setData, saveDataToDb, steps, service, isCustomGif
   const Header = () => <div className='d-flex justify-content-between mx-3 mt-3'>
     <Logo />
     <div className='mt-5' style={{marginLeft: '-101px'}}>
-      <h4 className='text-muted mb-1'>You picked:</h4>
+      <h5 className='text-muted'>You picked:</h5>
       <BigBtnEmotion emotion={emotion} onClick={backHandling} />
     </div>
     <Menu>X% complete</Menu>
