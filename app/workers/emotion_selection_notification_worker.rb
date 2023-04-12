912f2d31dddfd8a30ef5dcfc838d9f3425f600d0
VitaliiShevchenko <iksel@ukr.net>
2023-04-12 21:28:45 +0300

    CI-71: Changed in regards to reviewer and QA
    # Conflicts:
    #	app/assets/stylesheets/main.scss

--- app/workers/emotion_selection_notification_worker.rb
+++ app/workers/emotion_selection_notification_worker.rb
@@ -9,14 +9,14 @@ class EmotionSelectionNotificationWorker
   def run_notification
     return unless Date.current.strftime('%A').casecmp?(ENV.fetch('DAY_TO_SEND_INVITES'))
 
-    @time_period.update(due_date: Date.current)
+    time_period.update(due_date: Date.current)
     run_notification!
   end
 
   private
 
   def run_notification!
-    @users.each { |user| UserEmailMailer.response_invite(user, @time_period).deliver_now }
+    @users.each { |user| UserEmailMailer.response_invite(user, time_period).deliver_now }
   end
 
   def find_or_create_time_period
