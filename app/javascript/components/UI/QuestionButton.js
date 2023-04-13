5b4787ed5e359b0e4f5e21e85b1f3f40ff25360f
VitaliiShevchenko <iksel@ukr.net>
2023-04-13 17:02:00 +0300

    CI-158: Fixed conflict

--- app/javascript/components/UI/QuestionButton.js
+++ app/javascript/components/UI/QuestionButton.js
@@ -5,7 +5,7 @@ import help_icon from "../../../assets/images/help.svg"
 const QuestionButton = () => {
   return (
     <NavLink style={{position: 'absolute', right: 47, top: 756 }} to="mailto: support@vibereport.app">
-      <img  src={QuestionMark} alt="Question" />
+      <img  src={help_icon} alt="Question" />
 
     </NavLink>
   );
