58672c351268048e53e7c5db87f87ee07c328d9c
Maryna-Harasko <mngarasko@gmail.com>
2023-04-18 11:14:24 +0300

    CI-160 add some elements

--- app/javascript/components/Pages/Icebreaker.js
+++ app/javascript/components/Pages/Icebreaker.js
@@ -1,4 +1,6 @@
 import React, {Fragment} from 'react';
+import Form from 'react-bootstrap/Form'
+import 'icebreaker.scss';
 import {backHandling} from "../helpers/helpers";
 import {Wrapper, BtnBack, BtnNext, Header, ShoutOutIcon, HelpIcon} from "../UI/ShareContent";
 
@@ -12,10 +14,11 @@ const Icebreaker = ({data, setData, saveDataToDb, steps, service}) => {
   return (
     <Fragment>
       <Wrapper>
-        <Header />
-        <div className='d-flex justify-content-center'>
-          <h1>Kick back, relax.</h1>
-          <h1>Time for question of the week!</h1>
+        <Header/>
+        <div className='d-flex justify-content-center flex-column icebreaker'>
+          <h1 className='mb-0'>Kick back, relax.</h1>
+          <h1 className='mb-3'>Time for question of the week!</h1>
+          <h2>Brought to us by @mike</h2>
         </div>
         <div className='d-flex justify-content-between m-3'>
           <ShoutOutIcon />
