aa3facd3db6cc5d2e1a5aeba61158e89bcbf0136
LyubovP <lyubovpidoshva@mail.com>
2023-06-20 17:11:22 +0300

    CI-439: Ensure consistency between verbatims and counts in the Teams tab in Active Admin.

--- app/reports/teammate_engagement_count.rb
+++ app/reports/teammate_engagement_count.rb
@@ -11,19 +11,37 @@ class TeammateEngagementCount < AdminReport
   private
 
   def receive_teammate_engagement_count
-    teammate_ids = @team ? @team.users.ids : User.ids
-    shoutout_count(teammate_ids) + celebrate_comments_count(teammate_ids)
+    return 0 unless @team
+
+    team_member_ids = receive_team_members.map { |member| member[0] }
+    team_member_names = receive_team_members.map { |member| member[1] }
+
+    shoutouts = filter_comments(receive_shoutouts, team_member_ids, team_member_names)
+    celebrate_comments = filter_comments(receive_celebrate_comments, team_member_ids, team_member_names)
+
+    shoutouts.count + celebrate_comments.count
   end
 
-  def shoutout_count(teammate_ids)
-    Shoutout.where(user_id: teammate_ids, time_period_id: @time_periods).count
+  def receive_shoutouts
+    Shoutout.where(time_period_id: @time_periods).pluck(:rich_text)
   end
 
-  def celebrate_comments_count(teammate_ids)
-    Response.joins(:user)
-            .where(users: { id: teammate_ids })
+  def receive_celebrate_comments
+    Response.where(time_period_id: @time_periods)
             .where.not(celebrate_comment: nil)
-            .where(time_period_id: @time_periods)
-            .count
+            .pluck(:celebrate_comment)
+  end
+
+  def receive_team_members
+    User.joins(:user_teams)
+        .where(user_teams: { team_id: @team.id })
+        .pluck(:id, Arel.sql("first_name || ' ' || last_name"))
+  end
+
+  def filter_comments(comments, team_member_ids, team_member_names)
+    comments.select do |comment|
+      user_ids = Response.celebrate_user_ids_from_comment(comment)
+      (user_ids & team_member_ids).any? || team_member_names.any? { |name| comment.include?(name) }
+    end
   end
 end
