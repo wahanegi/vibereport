caf81106452fdadb8b796d65ef4d5c5b0c3bb9d5
Maryna-Harasko <mngarasko@gmail.com>
2023-07-07 17:51:01 +0300

    CI-479: fixed redirect on sea result page

--- app/javascript/components/UI/Menu.js
+++ app/javascript/components/UI/Menu.js
@@ -32,7 +32,7 @@ const Menu = ({ className = '', data, steps, draft, handleSaveDraft, prevId = nu
   const dropdownRef = useRef(null);
   const alertTitleLogout = "<div class='color-black'>Are you sure you <br/>  want to log out?</div>"
   const id = data?.response?.id || prevId
-  const lastStep = steps[steps.length - 1];
+  const lastStep = prevId ? 'result' : steps[steps.length - 1] ;
   const isLastStepDisabled = lastStep === 'emotion-entry' || lastStep === 'emotion-selection-web' || lastStep === 'result';
 
   useEffect(() => {
