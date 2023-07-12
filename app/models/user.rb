ef3ae99ee6f8c825dc0e209d2228a8d74c1a269a
LyubovP <lyubovpidoshva@mail.com>
2023-07-12 14:40:10 +0300

    CI-474: Save User's /Team's names without spaces at the beginning/end of their description

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,7 @@ class User < ApplicationRecord
   has_many :notifications, dependent: :destroy
   has_many :user_teams, dependent: :destroy
   has_many :teams, through: :user_teams
+  before_validation :strip_first_name_last_name
 
   MAX_NAME_LENGTH = 15
 
@@ -51,5 +52,10 @@ class User < ApplicationRecord
 
   def password_required?
     new_record? || password.present?
+  end
+
+  def strip_first_name_last_name
+    self.first_name = self.first_name.strip unless self.first_name.nil?
+    self.last_name = self.last_name.strip unless self.last_name.nil?
   end
 end
