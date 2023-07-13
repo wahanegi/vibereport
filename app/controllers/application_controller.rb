3421353183266c1da1768b1f95c14b97a3856d85
LyubovP <lyubovpidoshva@mail.com>
2023-07-13 13:57:07 +0300

    CI-483: CORS Issue: Fonts blocked on production server when herokuapp domain used

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_proper_subdomain, if: -> { request.get? }
+
+  private
+
+  def ensure_proper_subdomain
+    if ENV['DOMAIN_URL'].present? && request.host_with_port != ENV['DOMAIN_URL']
+      redirect_to [request.protocol, ENV['DOMAIN_URL'], request.fullpath].join
+    end
+  end
 end
