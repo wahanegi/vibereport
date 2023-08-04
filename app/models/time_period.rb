0f068237a4fab451d2a263d0c60a9896a93953d0
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-08-04 12:18:49 +0100

    CI-500: changes after review

--- app/models/time_period.rb
+++ app/models/time_period.rb
@@ -53,17 +53,17 @@ class TimePeriod < ApplicationRecord
     def create_time_period
       return if current_time_period.present?
 
-      start_date = Date.current.beginning_of_week(start_date_wday)
+      start_date = Date.current.beginning_of_week(day_that_week_starts)
       end_date = start_date + 6.days
-      due_date = (start_date..end_date).find { |date| date.strftime('%A').downcase.to_sym == due_date_wday }
+      due_date = (start_date..end_date).find { |date| date.strftime('%A').downcase.to_sym == day_to_send_invites }
       TimePeriod.create!(start_date:, end_date:, due_date:)
     end
 
-    def start_date_wday
+    def day_that_week_starts
       ENV['START_WEEK_DAY']&.to_sym || :monday
     end
 
-    def due_date_wday
+    def day_to_send_invites
       ENV['DAY_TO_SEND_INVITES']&.to_sym || :friday
     end
   end
