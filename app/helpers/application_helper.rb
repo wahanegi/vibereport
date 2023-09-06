ef2029ff9d29390c705525bccd0a83d535982fe9
SerhiiBorozenets <serhiiborozenets@gmail.com>
2023-09-06 12:25:19 +0100

    CI-524: fix rspec 2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,8 +8,8 @@ module ApplicationHelper
   end
 
   def check_period?
-    start_day = ENV['START_WEEK_DAY'].capitalize
-    end_day = ENV['DAY_TO_SEND_INVITES'].capitalize
+    start_day = (ENV['START_WEEK_DAY'] || 'tuesday').capitalize
+    end_day = (ENV['DAY_TO_SEND_INVITES'] || 'friday').capitalize
     current_day = Date.current.wday
     current_day >= Date::DAYNAMES.index(start_day) && current_day < Date::DAYNAMES.index(end_day)
   end
