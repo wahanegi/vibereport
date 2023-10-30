9ae7e535adc5b0285c79f3ec7604cf5a7615061e
LyubovP <lyubovpidoshva@mail.com>
2023-10-30 16:22:25 +0200

    Fix issue codeclimate

--- app/controllers/devise/passwordless/sessions_controller.rb
+++ app/controllers/devise/passwordless/sessions_controller.rb
@@ -4,9 +4,7 @@ class Devise::Passwordless::SessionsController < Devise::SessionsController
 
   def create
     self.resource = resource_class.find_by(email: create_params[:email])
-    if self.resource
-      resource.send_magic_link(remember_me: true)
-    end
+    resource.send_magic_link(remember_me: true) if self.resource
 
     self.resource = resource_class.new(create_params)
     redirect_to sent_path
