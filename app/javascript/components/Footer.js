b644c96493a1d0a97191f423b51e3e4a8e6d466e
Vitalii Bakun <crossdoh@gmail.com>
2025-01-27 13:59:15 +0200

    VIB-12: Fix Footer component

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -3,14 +3,14 @@ import ShoutoutButton from './UI/ShoutoutButton';
 import QuestionButton from './UI/QuestionButton';
 
 const Footer = ({ data, setData, hideShoutout }) => {
-  return (
-    <footer className="d-flex w-100 p-3 position-fixed bottom-0 left-0">
-      <div className="d-flex justify-content-between align-items-center w-100">
+  return <>
+      <div className="position-fixed z-1 left-px-10 bottom-px-10">
         <ShoutoutButton data={data} setData={setData} hideShoutout={hideShoutout} />
+      </div>
+      <div className="position-fixed z-1 right-px-10 bottom-px-10">
         <QuestionButton data={data} />
       </div>
-    </footer>
-  )
+  </>
 };
 
 export default Footer;
