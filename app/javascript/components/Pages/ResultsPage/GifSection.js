67ca30bc8e0ccca27a7a612ee18b25dbbaf26933
Vitalii Bakun <crossdoh@gmail.com>
2025-02-05 18:34:19 +0200

    VIB-18: Fix EmotionSection.js and GifSection.js

--- app/javascript/components/Pages/ResultsPage/GifSection.js
+++ app/javascript/components/Pages/ResultsPage/GifSection.js
@@ -25,18 +25,18 @@ const GifSection = ({ gifs, nextTimePeriod, isMinUsersResponses }) => {
   const gifItems = gifs.sort((a, b) => a.image.height - b.image.height).map((gif, index) => {
     return <div className='col align-items-center p-1' key={index}>
       <Tippy content={<div className={`btn btn-bubbles wb1 not-shadow tippy ${gif.emotion.category}`}>{gif.emotion.word}</div>}>
-        <img src={gif.image.src} alt={`gif ${index}`} />
+        <img src={gif.image.src} className="img-fluid" alt={`gif ${index}`} />
       </Tippy>
     </div>
   });
 
   return <>
     <div className='container'>
-      <div className="row">
+      <div className="row row-cols-lg-4 row-cols-md-2 row-cols-1">
         {gifItems}
       </div>
     </div>
-    <img src={PoweredBy} alt='PoweredBy' className={`big image-powered-by align-top`} />
+    <img src={PoweredBy} alt='PoweredBy' className="big image-powered-by align-top" />
   </>
 };
 
