4bbbe11c343b2036f47d47dee23f8b865262551d
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-02-17 11:24:52 +0200

    VIB-51: style time sheet mailer view

--- spec/mailers/previews/time_sheet_mailer_preview.rb
+++ spec/mailers/previews/time_sheet_mailer_preview.rb
@@ -3,6 +3,13 @@ class TimeSheetMailerPreview < ActionMailer::Preview
   def time_sheet_results_email
     time_period = TimePeriod.previous_time_period
     time_sheet_entries = time_period.time_sheet_entries
-    TimeSheetMailer.time_sheet_results_email(time_sheet_entries, time_period)
+    grouped_entries = group_and_sort_entries(time_sheet_entries)
+    TimeSheetMailer.time_sheet_results_email(grouped_entries, time_period)
+  end
+
+  private
+
+  def group_and_sort_entries(entries)
+    entries.group_by(&:project).sort_by { |project, _| project.code }
   end
 end
