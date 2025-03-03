5be315f9d77740c1ca0cb4b17c8c60fa1b31c36b
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-03-03 09:52:02 +0200

    VIB-66: Improve time input validation

--- app/javascript/components/UI/TimesheetRow.js
+++ app/javascript/components/UI/TimesheetRow.js
@@ -1,6 +1,6 @@
-import React from "react";
-import deleteIcon from "../../../assets/images/timesheet-row-delete.svg";
-import DropdownSelect from "./DropdownSelect";
+import React from 'react';
+import deleteIcon from '../../../assets/images/timesheet-row-delete.svg';
+import DropdownSelect from './DropdownSelect';
 
 // TODO - initOpt will be deleted after implementing data fetching from the backend.
 const initOpt = [
@@ -9,52 +9,63 @@ const initOpt = [
 ];
 
 const TimesheetRow = ({
-                        row,
-                        onDelete,
-                        onChangeRowData,
-                        optionsCompanyNames = initOpt,
-                        optionsProjectIds = initOpt,
-                        optionsProjectNames = initOpt
-                      }
-) => {
-  return <div className="d-flex gap-1 flex-row flex-nowrap align-items-center">
-    <div
-      className="timesheet-row d-flex gap-1 flex-row flex-nowrap align-items-center border border-royal-blue border-4 p-1">
-      <DropdownSelect id={"company_name"}
-                      options={optionsCompanyNames}
-                      selected={row.company}
-                      onChange={(value) => onChangeRowData(row.id, "company", value)}
-                      placeholder="Select..."
-                      className="select-company"
-      />
-      <DropdownSelect id={"project_id"}
-                      options={optionsProjectIds}
-                      selected={row.project_id}
-                      onChange={(value) => onChangeRowData(row.id, "project_id", value)}
-                      placeholder="Select..."
-                      className="select-project-id"
-      />
-      <DropdownSelect id={"project_name"}
-                      options={optionsProjectNames}
-                      selected={row.project_name}
-                      onChange={(value) => onChangeRowData(row.id, "project_name", value)}
-                      placeholder="Select..."
-                      className="select-project-name"
-      />
-      <input
-        type="number"
-        value={row.time}
-        onChange={(event) => onChangeRowData(row.id, "time", event.target.value)}
-        min={0}
-        placeholder=""
-        className="timesheet-row-input-time select-time shadow-none fs-5 text-gray-600 text fw-normal border-royal-blue border-4 border w-100 outline-focus-none"
-      />
+  row,
+  onDelete,
+  onChangeRowData,
+  optionsCompanyNames = initOpt,
+  optionsProjectIds = initOpt,
+  optionsProjectNames = initOpt,
+}) => {
+  const handleTimeChange = (event) => {
+    const value = event.target.value;
+    if (value === '' || (/^\d+$/.test(value) && parseInt(value, 10) > 0)) {
+      onChangeRowData(row.id, 'time', value);
+    }
+  };
+
+  return (
+    <div className="d-flex gap-1 flex-row flex-nowrap align-items-center">
+      <div className="timesheet-row d-flex gap-1 flex-row flex-nowrap align-items-center border border-royal-blue border-4 p-1">
+        <DropdownSelect
+          id={'company_name'}
+          options={optionsCompanyNames}
+          selected={row.company}
+          onChange={(value) => onChangeRowData(row.id, 'company', value)}
+          placeholder="Select..."
+          className="select-company"
+        />
+        <DropdownSelect
+          id={'project_id'}
+          options={optionsProjectIds}
+          selected={row.project_id}
+          onChange={(value) => onChangeRowData(row.id, 'project_id', value)}
+          placeholder="Select..."
+          className="select-project-id"
+        />
+        <DropdownSelect
+          id={'project_name'}
+          options={optionsProjectNames}
+          selected={row.project_name}
+          onChange={(value) => onChangeRowData(row.id, 'project_name', value)}
+          placeholder="Select..."
+          className="select-project-name"
+        />
+        <input
+          type="text"
+          value={row.time}
+          onChange={handleTimeChange}
+          placeholder=""
+          className="timesheet-row-input-time select-time shadow-none fs-5 text-gray-600 text fw-normal border-royal-blue border-4 border w-100 outline-focus-none"
+        />
+      </div>
+      <button
+        className="timesheet-row-button-delete bg-white border-4 border border-royal-blue rounded-circle"
+        onClick={() => onDelete(row.id)}
+      >
+        <img src={deleteIcon} alt="Delete icon" />
+      </button>
     </div>
-    <button className="timesheet-row-button-delete bg-white border-4 border border-royal-blue rounded-circle"
-            onClick={() => onDelete(row.id)}>
-      <img src={deleteIcon} alt="Delete icon"/>
-    </button>
-  </div>
+  );
 };
 
 export default TimesheetRow;
