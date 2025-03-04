53c801c9360347dbe635bb2d05a794117a1c6b96
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-03-05 00:15:26 +0200

    VIB-67: Update create action to handles an array of entries

--- app/controllers/api/v1/time_sheet_entries_controller.rb
+++ app/controllers/api/v1/time_sheet_entries_controller.rb
@@ -1,34 +1,27 @@
 class Api::V1::TimeSheetEntriesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_resources, only: :create
 
   def create
-    time_sheet_entry = TimeSheetEntry.new(time_sheet_entry_params)
+    saved_entries = []
 
-    if time_sheet_entry.save
-      render json: TimeSheetEntrySerializer.new(time_sheet_entry).serializable_hash, status: :created
-    else
-      render json: { errors: time_sheet_entry.errors.full_messages }, status: :unprocessable_entity
+    ActiveRecord::Base.transaction do
+      time_sheet_entries_params.each do |entry_params|
+        time_sheet_entry = TimeSheetEntry.new(entry_params)
+        unless time_sheet_entry.save
+          render json: { errors: time_sheet_entry.errors.full_messages }, status: :unprocessable_entity
+          raise ActiveRecord::Rollback
+        end
+
+        saved_entries << TimeSheetEntrySerializer.new(time_sheet_entry).serializable_hash
+      end
     end
+
+    render json: saved_entries, status: :created unless performed?
   end
 
   private
 
-  def time_sheet_entry_params
-    params.require(:time_sheet_entry).permit(:user_id, :project_id, :time_period_id, :total_hours)
-  end
-
-  def validate_resource(model, id_param, error_message)
-    id_value = params.dig(:time_sheet_entry, id_param)
-    unless id_value && model.exists?(id_value)
-      render json: { error: error_message }, status: :not_found
-      return
-    end
-  end
-  
-  def set_resources
-    validate_resource(User, :user_id, 'User not found')
-    validate_resource(Project, :project_id, 'Project not found')
-    validate_resource(TimePeriod, :time_period_id, 'Time Period not found')
+  def time_sheet_entries_params
+    params.permit(time_sheet_entries: %i[user_id project_id time_period_id total_hours])[:time_sheet_entries]
   end
 end
--- spec/requests/time_sheet_entries_spec.rb
+++ spec/requests/time_sheet_entries_spec.rb
@@ -3,127 +3,135 @@ require 'rails_helper'
 RSpec.describe 'TimeSheetEntries API', type: :request do
   let!(:user) { create(:user) }
   let!(:project) { create(:project) }
+  let!(:project2) { create(:project) }
   let!(:time_period) { create(:time_period) }
-
   let(:valid_params) do
     {
-      time_sheet_entry: {
-        user_id: user.id,
-        project_id: project.id,
-        time_period_id: time_period.id,
-        total_hours: 8
-      }
+      time_sheet_entries: [
+        {
+          user_id: user.id,
+          project_id: project.id,
+          time_period_id: time_period.id,
+          total_hours: 8
+        },
+        {
+          user_id: user.id,
+          project_id: project2.id,
+          time_period_id: time_period.id,
+          total_hours: 5
+        }
+      ]
     }
   end
+  let(:json_response) { response.parsed_body }
 
   before { sign_in(user) }
 
   describe 'POST /api/v1/time_sheet_entries tests' do
     context 'with valid parameters' do
-      it 'creates a record in the database and returns 201 Created' do
+      it 'creates records in the database and returns 201 Created' do
         expect do
           post '/api/v1/time_sheet_entries', params: valid_params
-        end.to change(TimeSheetEntry, :count).by(1)
+        end.to change(TimeSheetEntry, :count).by(2)
 
         expect(response).to have_http_status(:created)
-        json = response.parsed_body
-        expect(json['data']).not_to be_empty
+        expect(json_response).not_to be_empty
       end
     end
 
-    context 'if user_id is missing' do
-      it 'returns 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { user_id: nil })
+    context 'if user_id is not provided' do
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:user_id] = nil
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('User not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('User must exist')
       end
     end
 
     context 'if project_id is missing' do
-      it 'returns 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { project_id: nil })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:project_id] = nil
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('Project not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('Project must exist')
       end
     end
 
     context 'if time_period_id is missing' do
-      it 'returns 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { time_period_id: nil })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:time_period_id] = nil
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('Time Period not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('Time period must exist')
       end
     end
 
     context 'if total_hours is missing' do
-      it 'возвращает 422 Unprocessable Entity' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { total_hours: nil })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:total_hours] = nil
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
         expect(response).to have_http_status(:unprocessable_entity)
-        json = response.parsed_body
-        expect(json['errors']).to include("Total hours can't be blank")
+        expect(json_response['errors']).to include("Total hours can't be blank")
       end
     end
 
     context 'if total_hours is negative' do
-      it 'returns 422 Unprocessable Entity' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { total_hours: -1 })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:total_hours] = -1
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
         expect(response).to have_http_status(:unprocessable_entity)
-        json = response.parsed_body
-        expect(json['errors']).to include('Total hours must be greater than or equal to 0')
+        expect(json_response['errors']).to include('Total hours must be greater than or equal to 0')
       end
     end
 
     context 'if a non-existent user_id is passed' do
-      it 'возвращает 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { user_id: 99_999 })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:user_id] = 'invalid_id'
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('User not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('User must exist')
       end
     end
 
     context 'if a non-existent project_id is passed' do
-      it 'возвращает 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { project_id: 99_999 })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:project_id] = 'invalid_id'
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('Project not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('Project must exist')
       end
     end
 
     context 'if a non-existent time_period_id is passed' do
-      it 'возвращает 404 Not Found' do
-        invalid_params = valid_params.deep_merge(time_sheet_entry: { time_period_id: 99_999 })
+      it 'does not create any records and returns 422 Unprocessable Entity' do
+        invalid_params = valid_params.deep_dup
+        invalid_params[:time_sheet_entries][0][:time_period_id] = 'invalid_id'
 
         post '/api/v1/time_sheet_entries', params: invalid_params
 
-        expect(response).to have_http_status(:not_found)
-        json = response.parsed_body
-        expect(json['error']).to eq('Time Period not found')
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(json_response['errors']).to include('Time period must exist')
       end
     end
   end
