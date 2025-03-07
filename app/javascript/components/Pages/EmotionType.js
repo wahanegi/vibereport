a036672521f4262141a655177da5408b87391b97
AndrewRiabets <79791263+AndrewRiabets@users.noreply.github.com>
2025-03-07 11:12:26 +0200

    VIB-87 update mb EmotionType

--- app/javascript/components/Pages/EmotionType.js
+++ app/javascript/components/Pages/EmotionType.js
@@ -77,7 +77,7 @@ const EmotionType = ({
         handleSaveDraft={handleSaveDraft}
       >
         <div className="container pt-10">
-          <div className="row justify-content-center">
+          <div className="row justify-content-center mt-1">
             <div className="col-auto">
               <div className="text-center mt-1 mb-9">
                 <BigBtnEmotion
