2f77d4d07f177e7fbc0cddb961b2e04bae20a3c2
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-03-24 13:54:44 +0000

    VIB-107: The Shoutout button is not scroll-locked on all pages of the application.

--- app/javascript/components/Layout.js
+++ app/javascript/components/Layout.js
@@ -1,36 +1,41 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
+import ShoutoutButton from './UI/ShoutoutButton';
 
 const Layout = ({
-  children,
-  data,
-  setData,
-  numShoutouts = 0,
-  isMoveShoutout,
-  saveDataToDb,
-  steps,
-  draft,
-  disabled,
-  hideBottom = false,
-  isResult = false,
-  hideShoutout = false,
-  handleSaveDraft,
-}) => {
+                  children,
+                  data,
+                  setData,
+                  numShoutouts = 0,
+                  saveDataToDb,
+                  steps,
+                  draft,
+                  isResult = false,
+                  hideShoutout = false,
+                  handleSaveDraft,
+                }) => {
+  const location = useLocation();
+  const isRecognitionPage = location.pathname.match('recognition');
+
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Header data={data} steps={steps} draft={draft} handleSaveDraft={handleSaveDraft} saveDataToDb={saveDataToDb} isResult={isResult} />
-      <main className="flex-grow-1 d-flex justify-content-center align-items-start" role="main">
-        {children}
-      </main>
-      <Footer
-        data={data}
-        setData={setData}
-        hideShoutout={hideShoutout}
-        numShoutouts={numShoutouts}
-        isMoveShoutout={isMoveShoutout}
-      />
-    </div>
+    <>
+      <Header data={data} steps={steps} draft={draft} handleSaveDraft={handleSaveDraft} saveDataToDb={saveDataToDb}
+              isResult={isResult}/>
+
+      <div className={'position-relative'}>
+        <main
+          className="flex-grow-1 d-flex justify-content-center align-items-start fixed-button-offset"
+          role="main">
+          {children}
+        </main>
+
+        <div className={'position-fixed bottom-0 end-0 pb-1 pe-1'}>
+          {!isRecognitionPage &&
+            <ShoutoutButton data={data} setData={setData} hideShoutout={hideShoutout} num={numShoutouts}/>}
+        </div>
+      </div>
+    </>
   );
 };
 
