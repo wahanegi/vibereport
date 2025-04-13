d2257f7e5b7afd3ffb8ee58bbf49f7e89d0d05e8
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-04-13 18:21:20 +0300

    VIB-128: Add billable hours limit to the time_sheet_entry model

--- app/models/time_sheet_entry.rb
+++ app/models/time_sheet_entry.rb
@@ -23,11 +23,17 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class TimeSheetEntry < ApplicationRecord
+  MAX_BILLABLE_HOURS = 40
   belongs_to :user
   belongs_to :project
   belongs_to :time_period
 
   validates :total_hours, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :billable_hours_limit, if: -> { project&.billable? }
+
+  scope :billable, lambda {
+    joins(:project).where(projects: { usage: 'billable' })
+  }
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[id total_hours project_id time_period_id user_id]
@@ -35,5 +41,22 @@ class TimeSheetEntry < ApplicationRecord
 
   def self.ransackable_associations(_auth_object = nil)
     %w[project time_period user]
+  end
+
+  private
+
+  def existing_billable_hours
+    TimeSheetEntry
+      .billable
+      .where(user_id: user.id, time_period_id: time_period.id)
+      .where.not(id: id)
+      .sum(:total_hours)
+  end
+
+  def billable_hours_limit
+    total_billable_hours = existing_billable_hours + (total_hours || 0)
+    return unless total_billable_hours > MAX_BILLABLE_HOURS
+
+    errors.add(:total_hours, "Billable projects may not exceed #{MAX_BILLABLE_HOURS} hours in a work week")
   end
 end
