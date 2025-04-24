59bf0a57a0c9aace45c78d07af3197c9d991346d
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2025-04-24 11:42:16 +0300

    VIB-149: Add mailer for shoutouts that get reactions

---
+++ app/mailers/emoji_reactions_mailer.rb
@@ -0,0 +1,21 @@
+class EmojiReactionsMailer < ApplicationMailer
+  def emoji_reaction_email(author, messages, time_period)
+    @time_period = time_period
+    @author = author
+    @messages = messages
+    @url = ENV.fetch('DOMAIN_URL', nil)
+
+    mail(to: @author.email, subject: random_emoji_reaction_subject)
+  end
+
+  private
+
+  def random_emoji_reaction_subject
+    [
+      'People are reacting to your shoutouts',
+      'Your shoutouts are receiving attention',
+      'Someone thinks your shoutouts are awesome',
+      'Your shoutouts are being appreciated'
+    ].sample
+  end
+end
