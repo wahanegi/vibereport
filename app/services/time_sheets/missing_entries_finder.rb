340645dbcfb7587d2313b8fa98320949fc90d969
IvanRuskevych <fullstack.dev.goit@gmail.com>
2026-02-09 11:04:06 +0200

    VB-2: add service MissingEntriesFinder

---
+++ app/services/time_sheets/missing_entries_finder.rb
@@ -0,0 +1,39 @@
+module TimeSheets
+  class MissingEntriesFinder
+    def initialize(time_periods: TimePeriod.all)
+      @time_periods = time_periods
+    end
+
+    def call
+      result = {}
+
+      eligible_users.find_each do |user|
+        missing_periods = missing_periods_for(user)
+        result[user] = missing_periods if missing_periods.any?
+      end
+
+      result
+    end
+
+    private
+
+    def eligible_users
+      User
+        .joins(user_teams: :team)
+        .where(teams: { timesheet_enabled: true })
+        .where(opt_out: false)
+        .distinct
+    end
+
+    def missing_periods_for(user)
+      @time_periods
+        .where
+        .not(
+          id: TimeSheetEntry
+                .where(user_id: user.id)
+                .select(:time_period_id)
+        )
+        .ordered
+    end
+  end
+end
