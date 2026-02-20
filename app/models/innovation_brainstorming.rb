e8b84c5519bb1c7f57a6313a350dfe6dc50862a7
IvanRuskevych <fullstack.dev.goit@gmail.com>
2026-02-20 12:24:24 +0200

    VB-4: add InnovationTopic and InnovationBrainstorming models

---
+++ app/models/innovation_brainstorming.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: innovation_brainstormings
+#
+#  id                  :bigint           not null, primary key
+#  brainstorming_body  :text             not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  innovation_topic_id :bigint           not null
+#  user_id             :bigint           not null
+#
+# Indexes
+#
+#  index_unique_brainstorm_on_user_and_topic               (user_id,innovation_topic_id) UNIQUE
+#  index_innovation_brainstormings_on_innovation_topic_id  (innovation_topic_id)
+#  index_innovation_brainstormings_on_user_id              (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (innovation_topic_id => innovation_topics.id)
+#  fk_rails_...  (user_id => users.id)
+#
+class InnovationBrainstorming < ApplicationRecord
+  belongs_to :user
+  belongs_to :innovation_topic
+
+  has_one :response, dependent: :nullify
+
+  validates :brainstorming_body, presence: true
+  validates :user_id, uniqueness: { scope: :innovation_topic_id, message: "can submit only one brainstorming per topic" }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id brainstorming_body innovation_topic_id user_id created_at updated_at deleted_at]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[innovation_topic user response]
+  end
+
+end
