ddb631c6d4918be140dc3963acaba29b514c6902
IvanRuskevych <fullstack.dev.goit@gmail.com>
2026-02-20 18:50:59 +0200

    VB-4: add innovation_topic and innovation_brainstorming associations to Response with tests and factory updates

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,42 +2,48 @@
 #
 # Table name: responses
 #
-#  id                     :bigint           not null, primary key
-#  celebrate_comment      :string
-#  comment                :text
-#  completed_at           :date
-#  draft                  :boolean          default(FALSE), not null
-#  gif                    :jsonb
-#  not_working            :boolean          default(FALSE)
-#  notices                :jsonb
-#  productivity           :integer
-#  productivity_comment   :text
-#  rating                 :integer
-#  steps                  :string
-#  created_at             :datetime         not null
-#  updated_at             :datetime         not null
-#  emotion_id             :bigint
-#  fun_question_answer_id :bigint
-#  fun_question_id        :bigint
-#  shoutout_id            :bigint
-#  time_period_id         :bigint           not null
-#  user_id                :bigint           not null
+#  id                          :bigint           not null, primary key
+#  celebrate_comment           :string
+#  comment                     :text
+#  completed_at                :date
+#  draft                       :boolean          default(FALSE), not null
+#  gif                         :jsonb
+#  not_working                 :boolean          default(FALSE)
+#  notices                     :jsonb
+#  productivity                :integer
+#  productivity_comment        :text
+#  rating                      :integer
+#  steps                       :string
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
+#  emotion_id                  :bigint
+#  fun_question_answer_id      :bigint
+#  fun_question_id             :bigint
+#  innovation_brainstorming_id :bigint
+#  innovation_topic_id         :bigint
+#  shoutout_id                 :bigint
+#  time_period_id              :bigint           not null
+#  user_id                     :bigint           not null
 #
 # Indexes
 #
-#  index_responses_on_emotion_id                  (emotion_id)
-#  index_responses_on_fun_question_answer_id      (fun_question_answer_id)
-#  index_responses_on_fun_question_id             (fun_question_id)
-#  index_responses_on_shoutout_id                 (shoutout_id)
-#  index_responses_on_time_period_id              (time_period_id)
-#  index_responses_on_user_id                     (user_id)
-#  index_responses_on_user_id_and_time_period_id  (user_id,time_period_id) UNIQUE
+#  index_responses_on_emotion_id                   (emotion_id)
+#  index_responses_on_fun_question_answer_id       (fun_question_answer_id)
+#  index_responses_on_fun_question_id              (fun_question_id)
+#  index_responses_on_innovation_brainstorming_id  (innovation_brainstorming_id)
+#  index_responses_on_innovation_topic_id          (innovation_topic_id)
+#  index_responses_on_shoutout_id                  (shoutout_id)
+#  index_responses_on_time_period_id               (time_period_id)
+#  index_responses_on_user_id                      (user_id)
+#  index_responses_on_user_id_and_time_period_id   (user_id,time_period_id) UNIQUE
 #
 # Foreign Keys
 #
 #  fk_rails_...  (emotion_id => emotions.id)
 #  fk_rails_...  (fun_question_answer_id => fun_question_answers.id)
 #  fk_rails_...  (fun_question_id => fun_questions.id)
+#  fk_rails_...  (innovation_brainstorming_id => innovation_brainstormings.id)
+#  fk_rails_...  (innovation_topic_id => innovation_topics.id)
 #  fk_rails_...  (shoutout_id => shoutouts.id)
 #  fk_rails_...  (time_period_id => time_periods.id)
 #  fk_rails_...  (user_id => users.id)
@@ -48,6 +54,8 @@ class Response < ApplicationRecord
   belongs_to :user
   belongs_to :fun_question, optional: true
   belongs_to :fun_question_answer, optional: true
+  belongs_to :innovation_topic, optional: true
+  belongs_to :innovation_brainstorming, optional: true
 
   validates :user_id, uniqueness: { scope: :time_period_id }
   validates :steps, presence: true
