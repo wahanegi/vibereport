a09e368e6d34e4aca856ade96db7f2e2decff4c5
AndrewRiabets <andrewriabets@gmail.com>
2026-02-27 12:05:00 +0200

    VB-7 Add SignedLinks::Builder and migrate DirectTimesheetEntryBuilder

--- spec/services/signed_links/direct_timesheet_entry_builder_spec.rb
+++ spec/services/signed_links/direct_timesheet_entry_builder_spec.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe TimeSheets::DirectLinkBuilder do
+RSpec.describe SignedLinks::DirectTimesheetEntryBuilder do
   include ActiveSupport::Testing::TimeHelpers
 
   let(:user) { create(:user) }
