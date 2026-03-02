dd803fb31498cec0f0c3c761fb68c0ea07f652e7
IvanRuskevych <fullstack.dev.goit@gmail.com>
2026-03-02 18:52:38 +0200

    VB-6: add innovation-topic and innovation-brainstorming routes with fallback

--- app/javascript/components/Pages/Recognition.js
+++ app/javascript/components/Pages/Recognition.js
@@ -39,7 +39,7 @@ const Recognition = ({data, setData, saveDataToDb, steps, service, draft}) => {
       steps.push('causes-to-celebrate')
       saveDataToDb(steps, {draft: false})
     } else
-      steps.push('icebreaker-answer')
+      steps.push('innovation-brainstorming')
     saveDataToDb(steps, {draft: false})
   }
   const skipHandling = () => {
@@ -70,38 +70,30 @@ const Recognition = ({data, setData, saveDataToDb, steps, service, draft}) => {
 
   const output = (shoutOuts) => {
     return (
-        <ul className="d-flex flex-column gap-2 py-2 list-unstyled">
-          {shoutOuts.map(shoutOut => (
-              <li
-                  className="bg-light position-relative align-middle border rounded-4 border-4 border-primary p-1 text-break pe-10 w-100"
-                  key={shoutOut.id}>
-                <p className="text-start fs-7 fs-sm-5 fs-md-4 fw-semibold m-0 text-truncate">{parse(
-                    shoutOut.rich_text)}</p>
-                <div
-                    className="position-absolute top-50 end-0 translate-middle-y d-flex">
-                  <img id={shoutOut.id} src={edit_pencil} alt="pencil"
-                       className="pointer" onClick={editHandling}/>
-                  <span className="expand-link"
-                        onMouseEnter={(e) =>
-                            e.currentTarget.querySelector('.trash').
-                                classList.
-                                add('d-none') ||
-                            e.currentTarget.querySelector('.trashRed').
-                                classList.
-                                remove('d-none')}
-                        onMouseLeave={(e) =>
-                            e.currentTarget.querySelector('.trash').
-                                classList.
-                                remove('d-none') ||
-                            e.currentTarget.querySelector('.trashRed').
-                                classList.
-                                add('d-none')}
-                  >
+      <ul className="d-flex flex-column gap-2 py-2 list-unstyled">
+        {shoutOuts.map(shoutOut => (
+          <li
+            className="bg-light position-relative align-middle border rounded-4 border-4 border-primary p-1 text-break pe-10 w-100"
+            key={shoutOut.id}>
+            <p className="text-start fs-7 fs-sm-5 fs-md-4 fw-semibold m-0 text-truncate">{parse(
+              shoutOut.rich_text)}</p>
+            <div
+              className="position-absolute top-50 end-0 translate-middle-y d-flex">
+              <img id={shoutOut.id} src={edit_pencil} alt="pencil"
+                   className="pointer" onClick={editHandling} />
+              <span className="expand-link"
+                    onMouseEnter={(e) =>
+                      e.currentTarget.querySelector('.trash').classList.add('d-none') ||
+                      e.currentTarget.querySelector('.trashRed').classList.remove('d-none')}
+                    onMouseLeave={(e) =>
+                      e.currentTarget.querySelector('.trash').classList.remove('d-none') ||
+                      e.currentTarget.querySelector('.trashRed').classList.add('d-none')}
+              >
             <img src={trash} alt="trash" className="trash"
-                 onClick={trashHandling}/>
+                 onClick={trashHandling} />
             <img id={'trashRed' + shoutOut.id} src={trashRed} alt="trash"
                  className="trashRed d-none"
-                 onClick={trashHandling}/>
+                 onClick={trashHandling} />
           </span>
             </div>
           </li>
@@ -126,12 +118,12 @@ const Recognition = ({data, setData, saveDataToDb, steps, service, draft}) => {
         {!numShoutOuts &&
           <div className='mb-6'>
             <h2 className='fs-md-4 text-black mb-4'>
-              Consider giving members of your team a <br/>
+              Consider giving members of your team a <br />
               Shoutout to show your appreciation.
             </h2>
             <div className='shoutout-container position-relative'>
               <ShoutoutButton data={data} setData={setData} isDraft={isDraft} saveDataToDb={saveDataToDb} steps={steps}
-                              draft={isDraft} handleSaveDraft={handleSaveDraft} isMove={true}/>
+                              draft={isDraft} handleSaveDraft={handleSaveDraft} isMove={true} />
             </div>
             <p className='gray-600 mt-1 fw-bold'>Click to give a Shoutout!</p>
           </div>
@@ -145,34 +137,34 @@ const Recognition = ({data, setData, saveDataToDb, steps, service, draft}) => {
           </div>
         }
 
-          {!!numShoutOuts &&
-              <div className="d-flex d-sm-flex flex-column mx-auto"
-                   style={{maxWidth: '884px'}}>
-                {shoutOutForm.status &&
-                    <ShoutoutModal shoutOutForm={shoutOutForm}
-                                   setShoutOutForm={setShoutOutForm}
-                                   data={data}
-                                   setData={setData}
-                                   editObj={shoutOutForm.editObj}/>}
-                <div
-                    className="container justify-content-center border border-4 rounded-4 border-primary field-shout-outs w-100">
-                  {output(shoutOuts)}
-                </div>
-                <p className="m-0 mt-4 fs-8 fs-md-7">Any more shoutouts to
-                  give?</p>
-                <div className={'shoutout-container-small position-relative'}>
-                  <ShoutoutButton data={data} setData={setData}
-                                  isDraft={isDraft} saveDataToDb={saveDataToDb}
-                                  steps={steps}
-                                  draft={isDraft}
-                                  handleSaveDraft={handleSaveDraft}
-                                  num={numShoutOuts} isMove={true}/>
-                </div>
-              </div>
-          }
+        {!!numShoutOuts &&
+          <div className="d-flex d-sm-flex flex-column mx-auto"
+               style={{maxWidth: '884px'}}>
+            {shoutOutForm.status &&
+              <ShoutoutModal shoutOutForm={shoutOutForm}
+                             setShoutOutForm={setShoutOutForm}
+                             data={data}
+                             setData={setData}
+                             editObj={shoutOutForm.editObj} />}
+            <div
+              className="container justify-content-center border border-4 rounded-4 border-primary field-shout-outs w-100">
+              {output(shoutOuts)}
+            </div>
+            <p className="m-0 mt-4 fs-8 fs-md-7">Any more shoutouts to
+              give?</p>
+            <div className={'shoutout-container-small position-relative'}>
+              <ShoutoutButton data={data} setData={setData}
+                              isDraft={isDraft} saveDataToDb={saveDataToDb}
+                              steps={steps}
+                              draft={isDraft}
+                              handleSaveDraft={handleSaveDraft}
+                              num={numShoutOuts} isMove={true} />
+            </div>
+          </div>
+        }
 
-        {isModal && <ShoutoutDelete onClose={onClose} data={data} setData={setData} idShoutout={idShoutout}/>}
-        <BlockLowerBtns skipHandling={skipHandling} nextHandling={nextHandling} isNext={!!numShoutOuts}/>
+        {isModal && <ShoutoutDelete onClose={onClose} data={data} setData={setData} idShoutout={idShoutout} />}
+        <BlockLowerBtns skipHandling={skipHandling} nextHandling={nextHandling} isNext={!!numShoutOuts} />
       </div>
     </Layout>
   );
--- app/javascript/components/ResponseFlow.js
+++ app/javascript/components/ResponseFlow.js
@@ -18,6 +18,8 @@ import EmotionType from "./Pages/EmotionType";
 import RatherNotSay from "./Pages/RatherNotSay/RatherNotSay";
 import SkipAhead from "./Pages/RatherNotSay/SkipAhead";
 import ResultsManager from "./Pages/ResultsPageManager";
+import InnovationBrainstorming from "./Pages/InnovationBrainstorming";
+import InnovationTopic from "./Pages/InnovationTopic";
 
 const ResponseFlow = ({step, data, setData, setIsShuffleEmotions}) => {
   const [isLoading, setIsLoading] = useState(false)
@@ -25,54 +27,58 @@ const ResponseFlow = ({step, data, setData, setIsShuffleEmotions}) => {
   const stepsArr = data.response.attributes.steps
   const prev_results_path = data.prev_results_path
   const navigate = useNavigate()
-  const service = {isLoading,  error , setIsLoading}
+  const service = {isLoading, error, setIsLoading}
   const draft = data.response.attributes.draft
   const mainPage = 'emotion-selection-web'
   const [go, setGo] = useState(null)
   const [isCustomGif, setIsCustomGif] = useState(false)
 
-  useEffect(()=>{
+  useEffect(() => {
     //a block to give permission to transition on a page which was point the browser address bar
     let lastStep = stepsArr.slice(-1).toString()
     let index = stepsArr.indexOf(step)
 
-    if ( stepsArr[0] !== mainPage ) {
+    if (stepsArr[0] !== mainPage) {
       saveDataToDb([mainPage])
     }
     if (index === -1) {
       navigate(`/${lastStep}`)
       setGo(lastStep)
-    } else if (index !== stepsArr.length-1) {
+    } else if (index !== stepsArr.length - 1) {
       saveDataToDb(stepsArr.slice(0, stepsArr.indexOf(step) + 1))
     } else {
       setGo(lastStep)
     }
-  },[data])
+  }, [data])
 
-  useEffect(()=> {
-      window.addEventListener('popstate', function(event) {
-        event.preventDefault()
-        const handlingSteps =( answer ) =>{
-          const stepsFromDBofServer = answer.response.attributes.steps
-          if (stepsFromDBofServer.length > 1) {
-            stepsFromDBofServer.pop()
-            setGo(stepsFromDBofServer.slice(-1).toString())
-            createOrUpdate( answer ,
-              { response:{ attributes:{
-                    emotion_id: answer.response.attributes.emotion_id,
-                            id: answer.response.attributes.id,
-                         steps: stepsFromDBofServer,
-                time_period_id: answer.time_period.id,
-                       user_id: answer.current_user.id
-                }}}, saveDataToAttributes)}
-          else{
-            window.location.replace(window.location.origin+`/${stepsFromDBofServer[0]}` );
-            setGo(stepsFromDBofServer[0])
-          }
+  useEffect(() => {
+    window.addEventListener('popstate', function (event) {
+      event.preventDefault()
+      const handlingSteps = (answer) => {
+        const stepsFromDBofServer = answer.response.attributes.steps
+        if (stepsFromDBofServer.length > 1) {
+          stepsFromDBofServer.pop()
+          setGo(stepsFromDBofServer.slice(-1).toString())
+          createOrUpdate(answer,
+            {
+              response: {
+                attributes: {
+                  emotion_id: answer.response.attributes.emotion_id,
+                  id: answer.response.attributes.id,
+                  steps: stepsFromDBofServer,
+                  time_period_id: answer.time_period.id,
+                  user_id: answer.current_user.id
+                }
+              }
+            }, saveDataToAttributes)
+        } else {
+          window.location.replace(window.location.origin + `/${stepsFromDBofServer[0]}`);
+          setGo(stepsFromDBofServer[0])
         }
-        apiRequest("GET", "", handlingSteps, ()=>{}, '/api/v1/emotions.json').catch(e=>setError(e))
-      });
-  },[])
+      }
+      apiRequest("GET", "", handlingSteps, () => {}, '/api/v1/emotions.json').catch(e => setError(e))
+    });
+  }, [])
 
   useEffect(() => {
     prev_results_path && navigate(prev_results_path)
@@ -82,35 +88,35 @@ const ResponseFlow = ({step, data, setData, setIsShuffleEmotions}) => {
   //*** **steps** - array with steps of user for update or save in DB
   //*** **addedData** - necessary data (and future data) for update or save in DB by using Response controller
   //*** Format addedData = **{key: value, ...., key(n): value(n)}**
-  const saveDataToDb = ( stepsArr, addedData = {}) => {
-    const dataRequest = {response:{ attributes: { steps: stepsArr,...addedData } } }
+  const saveDataToDb = (stepsArr, addedData = {}) => {
+    const dataRequest = {response: {attributes: {steps: stepsArr, ...addedData}}}
     setIsLoading(true)
-    createOrUpdate (data, dataRequest, saveDataToAttributes)
+    createOrUpdate(data, dataRequest, saveDataToAttributes)
   }
 
   const createOrUpdate = (data, dataRequest, saveDataToAttributes) => {
     const url = '/api/v1/responses/' + data.response.attributes.id
     data.response.attributes.emotion_id === undefined ?
       //create new record in the Response table
-      apiRequest("POST", dataRequest, saveDataToAttributes ).catch(e=>setError(e))
+      apiRequest("POST", dataRequest, saveDataToAttributes).catch(e => setError(e))
       :
-      apiRequest("PATCH", dataRequest, saveDataToAttributes, ()=>{}, url ).catch(e=>setError(e))
+      apiRequest("PATCH", dataRequest, saveDataToAttributes, () => {}, url).catch(e => setError(e))
   }
 
   //***  include received data from the apiRequest to the variable **:data (see :frontDATABASE in App)**
   // briefly: including data from server to the client-side database
-  const saveDataToAttributes =( receivedData ) =>{
+  const saveDataToAttributes = (receivedData) => {
     setIsLoading(false)
-    mergeData( receivedData, data, setData )
+    mergeData(receivedData, data, setData)
     let lastStep = receivedData.data.attributes.steps.slice(-1).toString()
-    setGo( lastStep )
-    navigate(`/${ lastStep }`)
+    setGo(lastStep)
+    navigate(`/${lastStep}`)
   }
 
   const componentMap = {
     'emotion-selection-web': <ListEmotions setIsShuffleEmotions={setIsShuffleEmotions} />,
-    'rather-not-say': <RatherNotSay/>,
-    'skip-ahead': <SkipAhead/>,
+    'rather-not-say': <RatherNotSay />,
+    'skip-ahead': <SkipAhead />,
     'emotion-entry': <EmotionEntry />,
     'emotion-type': <EmotionType />,
     'meme-selection': <MemeSelection isCustomGif={isCustomGif} setIsCustomGif={setIsCustomGif} />,
@@ -121,6 +127,8 @@ const ResponseFlow = ({step, data, setData, setIsShuffleEmotions}) => {
     'productivity-bad-follow-up': <ProductivityBadFollowUp />,
     'causes-to-celebrate': <CausesToCelebrate />,
     'recognition': <Recognition />,
+    'innovation-brainstorming': <InnovationBrainstorming />,
+    'innovation-topic': <InnovationTopic />,
     'icebreaker-answer': <IcebreakerAnswer />,
     'icebreaker-question': <IcebreakerQuestion />,
     'result-managers': <ResultsManager />,
@@ -135,7 +143,20 @@ const ResponseFlow = ({step, data, setData, setIsShuffleEmotions}) => {
     draft,
   };
 
-  const component = componentMap[go] || null;
+// fallback
+  let resolvedStep = go;
+  if (go === 'innovation-brainstorming' && !data.innovation_topic) {
+    resolvedStep = 'innovation-topic';
+  }
+
+// synchronize URL
+  useEffect(() => {
+    if (resolvedStep && resolvedStep !== go) {
+      navigate(`/${resolvedStep}`, {replace: true});
+    }
+  }, [resolvedStep, go, navigate]);
+
+  const component = componentMap[resolvedStep] || null;
 
   return component ? React.cloneElement(component, componentProps) : null;
 }
